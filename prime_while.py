f733aeca667516a3562961bce0e0a73d6a941a86
Libert Sin <libert@photoguraphy.com>
2022-03-06 06:13:10 +0000

    Update prime_for.py, prime_while.py

---
+++ prime_while.py
@@ -0,0 +1,21 @@
+import time
+
+a=3
+
+prime=[2]
+stime=time.time()
+
+while 1==1:
+	b=a-1
+	r=0
+	while b>=2:
+		if a%b ==0:
+			r=0
+			break
+		b-=1
+		r=1
+	if r==1:
+		prime.append(a)
+		print(prime[-1],'\t',time.time()-stime)
+	a+=1	
+
